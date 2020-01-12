37fe000a99d0424dca94f0f7d0286778d2906c3f
Martin Cole <martin_cole@urmc.rochester.edu>
2020-01-12 13:40:27 -0500

    Added conditional and fisher correlation options.

--- scripts/calculate_sc_fc_cor_map.py
+++ scripts/calculate_sc_fc_cor_map.py
@@ -26,6 +26,12 @@ def _build_args_parser():
     p.add_argument('--output', action='store', metavar='OUTPUT', required=True,
                    type=str, help='Path of the .npz file to save the output to.')
 
+    p.add_argument('--conditional', action='store', type=int, metavar='CONDITIONAL', default=None,
+                    help='If set greater than 1, correlations are calculated only on vertices with WM connections to at least n other vertices.')
+
+    p.add_argument('--fisher', action='store_true', dest='fisher', default=False,
+                    help='Perform a Fisher z-transform on the FC before calculating correlation.')
+
     p.add_argument('-f', action='store_true', dest='overwrite',
                    help='If set, overwrite files if they already exist.')
 
@@ -47,6 +53,9 @@ def main():
     if not isfile(args.mesh):
         parser.error('The file "{0}" must exist.'.format(args.mesh))
 
+    if not (args.conditional is None) and (args.conditional < 2):
+        parser.error('Minimum vector length for conditional correlations must be greater than 1')
+
     # make sure files are not accidently overwritten
     if isfile(args.output):
         if args.overwrite:
@@ -63,17 +72,25 @@ def main():
 
     logging.info('Calculating FC/SC correlation.')
 
-    fc = np.zeros((n,n))
-    fc[np.triu_indices(n, 0)] = np.load(args.fc_matrix)['fc']
+    # load sc matrix and make dense
     sc = np.array(sparse.load_npz(args.sc_matrix).todense())
 
-    result = np.zeros(n)
+    # load fc and convert to upper triangle matrix
+    fc = np.zeros((n,n))
+    fc[np.triu_indices(n, 0)] = np.load(args.fc_matrix)['fc']
+
+    # perform fisher transformation
+    if args.fisher:
+        fc = np.arctanh(fc)
 
     # calculate the FC using an upper triangular indexing scheme
+    result = np.zeros(n)
+
     for i in xrange(n):
-        lower = i-1
+        lower = i
         upper = i+1
         
+        # get the whole row from the triangular matrix
 	if i == 0:
             fc_row = fc[i, upper:]
             sc_row = sc[i, upper:]
@@ -84,17 +101,26 @@ def main():
             fc_row = np.concatenate((np.transpose(fc[0:lower, i]), fc[i, upper:]))
             sc_row = np.concatenate((np.transpose(sc[0:lower, i]), sc[i, upper:]))
 
+        if args.conditional > 1:
+            conditional_msk = sc_row > 0
+            
+            # if not enough connections, set correlation to 0 and move on
+            if (sc[i,i] == 0) or (np.sum(conditional_msk) < args.conditional):
+                result[i] = 0
+                continue
+            
+            fc_row = fc_row[conditional_msk]
+            sc_row = sc_row[conditional_msk]
+
+        # remove nans from the matrices (caused by unknown regions)
         mask_idx = ~np.isnan(fc_row)
 
+        # if all values in the row are nans, set the result to nan and move on
         if not np.any(mask_idx):
-            #result[i] = np.nan
-            result[i] = -2
+            result[i] = np.nan
             continue
 
         result[i] = np.corrcoef(fc_row[mask_idx], sc_row[mask_idx])[0,1]
-
-        if np.isnan(result[i]):
-            result[i] = -2
 
     # save the results
     np.savez_compressed(args.output, corr_map=result)
