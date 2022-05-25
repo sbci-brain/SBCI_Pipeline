c50a8da9d369e81d8f64e1d12774f52df6fead7b
Martin Robert Cole <mrcole@longleaf-login4.its.unc.edu>
2022-05-25 11:31:33 -0400

    Adding PSC to ABCD example

---
+++ ABCD_example/process_psc.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+IN=${1}
+OUT=${2}
+SCRIPTS=${3}
+
+# CHANGE LOCATION TO YOUR SOURCE FILE
+echo "Sourcing .bashrc"
+source ~/.bashrc-sbci
+
+# CHANGE LOCATION TO THE CONFIGURATION FILE FOR SBCI
+export SBCI_CONFIG=/nas/longleaf/home/mrcole/SBCI_Pipeline/abcd_pipeline/sbci_config
+
+# CHANGE FOR SPECIFIC SBATCH OPTIONS
+OPTIONS=""
+
+echo "Sourcing SBCI config file"
+source $SBCI_CONFIG
+
+echo $(which python)
+
+# helper function to return job id
+function sb() {
+   result="$(sbatch "$@")"
+   
+   if [[ "$result" =~ Submitted\ batch\ job\ ([0-9]+) ]]; then
+     echo "${BASH_REMATCH[1]}"
+   fi 
+}
+
+# create a unique job name prefix
+JID=$(uuidgen | tr '-' ' ' | awk {'print $1}')
+
+# get all subject names
+mapfile -t subjects < ${IN}
+
+# make sure there are subjects
+if [[ ${#subjects[@]} -eq 0 ]]; then
+    echo "no subjects found in ${IN}"
+    exit 1
+fi
+
+echo "Processing ${#subjects[@]} subject(s): ${JID}"
+
+ROOT=$(pwd)
+
+for i in $(seq 1 ${#subjects[@]}); do
+    idx=$((i - 1))
+
+    cd ${OUT}/${subjects[$idx]}
+
+    echo "Placing subject ${subjects[$idx]} in queue"
+    STEP1=$(sb $OPTIONS --time=48:00:00 --mem=15g --job-name=$JID.step1 \
+        --export=ALL,SBCI_CONFIG \
+        --output=psc_step1_tractography.log ${SCRIPTS}/psc_step1_tractography.sh)
+
+    cd ${ROOT}
+done
