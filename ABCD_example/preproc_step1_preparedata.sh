78993e656cec5f916bdf196fe676b200ce4e5d9e
Martin Robert Cole <martin_cole@urmc.rochester.edu>
2022-08-18 17:24:29 -0400

    ABCD data sometimes is split in two. Change scripts to merge them before processing

--- ABCD_example/preproc_step1_preparedata.sh
+++ ABCD_example/preproc_step1_preparedata.sh
@@ -9,8 +9,50 @@ cd ..
 
 # flip dMRI
 cd dwi
-mrconvert *_dwi.nii.gz -stride 1,2,3,4 flipped_data.nii.gz -force
-scil_convert_gradient_fsl_to_mrtrix.py *_dwi.bval *_dwi.bvec encoding.b  -f
+run_count=$(ls -f *[0-9]_dwi.nii.gz | wc -l)
+
+# some subjects have multiple runs (if so, merge them into one)
+if [ ${run_count} -gt 1 ]; then
+    # combine dwi image files
+    fslmerge -t dwi_all *[0-9]_dwi.nii.gz
+
+    # combine b-value files
+    for f in $(ls *[0-9]_dwi.bval); do 
+        read -d '' -r -a bvals < ${f}
+        length=${#bvals[@]}
+        echo -n ' '${bvals[@]:0:length} >> bval_all.bval
+    done
+
+    # combine b-vector files
+    for f in $(ls *[0-9]_dwi.bvec | sed 's/\..*$//'); do
+        read -d '' -r -a bvals < ${f}.bval
+        length=${#bvals[@]}
+        read -d '' -r -a bvecs < ${f}.bvec
+        echo -n ' '${bvecs[@]:0:length} >> bvecs_1
+        echo -n ' '${bvecs[@]:length:length} >> bvecs_2
+        echo -n ' '${bvecs[@]:length+length:length} >> bvecs_3
+    done
+
+    echo $(cat bvecs_1) > bvec_all.bvec
+    echo $(cat bvecs_2) >> bvec_all.bvec
+    echo $(cat bvecs_3) >> bvec_all.bvec
+
+    mrconvert dwi_all.nii.gz -stride 1,2,3,4 flipped_data.nii.gz -force
+    scil_convert_gradient_fsl_to_mrtrix.py bval_all.bval bvec_all.bvec encoding.b  -f
+
+    # remove intermediate files
+    rm -rf dwi_all.nii.gz
+    rm -rf bval_all.bval
+    rm -rf bvec_all.bvec
+    rm -rf bvecs_1 bvecs_2 bvecs_3
+fi
+
+# if only one subject, skip all the merging steps
+if [ ${run_count} -eq 1 ]; then
+    mrconvert *_dwi.nii.gz -stride 1,2,3,4 flipped_data.nii.gz -force
+    scil_convert_gradient_fsl_to_mrtrix.py *_dwi.bval *_dwi.bvec encoding.b  -f
+fi
+
 scil_flip_grad.py --mrtrix encoding.b encoding_x.b x -f
 scil_convert_gradient_mrtrix_to_fsl.py encoding_x.b flip_x.bval flip_x.bvec -f 
 cd ..
